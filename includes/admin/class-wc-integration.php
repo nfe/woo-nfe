34ac2380bdd9a5d613b4f6554036ee6bd7344742
Renato Alves <espellcaste@gmail.com>
2016-05-03 13:29:09 -0300

    integration settings fields added

--- includes/admin/class-wc-integration.php
+++ includes/admin/class-wc-integration.php
@@ -24,6 +24,7 @@ if ( ! class_exists( 'WC_Nfe_Integration' ) ) :
 			$this->plugin_id		  = 'nfe-woo-integration';
 			$this->id                 = 'nfe-woo';
 			$this->method_title 	  = __( 'Nfe Integration', 'nfe-woocommerce' );
+			$this->label 			  = __( 'Nfe Integration', 'nfe-woocommerce' );
 			$this->method_description = __( 'This is the Nfe.io integration/settings page.', 'nfe-woocommerce' );
 
 			// Load the settings.
@@ -46,12 +47,46 @@ if ( ! class_exists( 'WC_Nfe_Integration' ) ) :
 		 */
 		public function init_form_fields() {
 			$this->form_fields = array(
+				'nfe_enable' => array(
+					'title'             => __( 'Enable/Disable', 'nfe-woocommerce' ),
+					'type'              => 'checkbox',
+					'label'             => __( 'Enable Nfe.io', 'nfe-woocommerce' ),
+					'default'           => 'no',
+				),
 				'api_key' => array(
 					'title'             => __( 'API Key', 'nfe-woocommerce' ),
 					'type'              => 'text',
-					'description'       => __( 'Enter with your API Key. You can find this in you nfe.io profile in....', 'nfe-woocommerce' ),
-					'desc_tip'          => true,
-					'default'           => ''
+					'label'             => __( 'API Key', 'nfe-woocommerce' ),
+					'default'           => '',
+					'description'       => __( 'Enter your API Key. You can find this in you Nfe.io profile.', 'nfe-woocommerce' ),
+				),
+				'where_note' => array(
+					'title'             => __( 'Nfe.io Filling', 'nfe-woocommerce' ),
+					'type'              => 'select',
+					'label'             => __( 'Nfe.io Filling', 'nfe-woocommerce' ),
+					'default'           => 'before',
+					'options' 			=> array(
+						'after'     	=> __( 'After Checkout', 'nfe-woocommerce' ),
+						'before'    	=> __( 'Before Checkout', 'nfe-woocommerce' ),
+						'manual'    	=> __( 'Manual (Requires admin to issue)', 'nfe-woocommerce' )
+					),
+					'class'    			=> 'wc-enhanced-select',
+					'css'      			=> 'min-width:300px;',
+					'description'       => __( 'The place where the user must fill the Nfe.io information.', 'nfe-woocommerce' ),
+				),
+				'issue_past_notes' => array(
+					'title'             => __( 'Enable Retroactive Issue', 'nfe-woocommerce' ),
+					'type'              => 'checkbox',
+					'label'             => __( 'Enable to issue Nfe.io in past products', 'nfe-woocommerce' ),
+					'default'           => 'no',
+					'description'       => __( 'Enabling this allows users to issue nfe.io notes on bought products in the past.', 'nfe-woocommerce' ),
+				),
+				'send_email_copy' => array(
+					'title'             => __( 'Enable Safe Copy', 'nfe-woocommerce' ),
+					'type'              => 'checkbox',
+					'label'             => __( 'Enable or disable safe issue copy', 'nfe-woocommerce' ),
+					'default'           => 'no',
+					'description'       => __( 'If enabled, a copy of every note issued is sent to the blog admin email.', 'nfe-woocommerce' ),
 				),
 				'debug' => array(
 					'title'             => __( 'Debug Log', 'nfe-woocommerce' ),
@@ -59,7 +94,7 @@ if ( ! class_exists( 'WC_Nfe_Integration' ) ) :
 					'label'             => __( 'Enable logging', 'nfe-woocommerce' ),
 					'default'           => 'no',
 					'description'       => __( 'Log events such as API requests', 'nfe-woocommerce' ),
-				),
+				)
 			);
 		}
 
@@ -70,7 +105,9 @@ if ( ! class_exists( 'WC_Nfe_Integration' ) ) :
 		 */
 		public function sanitize_settings( $settings ) {
 			if ( isset( $settings ) && isset( $settings['api_key'] ) ) {
-				$settings['api_key'] = strtolower( $settings['api_key'] );
+				if ( strlen( $settings['api_key'] ) > 10 ) {
+					$settings['api_key'] = strtolower( $settings['api_key'] );
+				}
 			}
 
 			return $settings;
