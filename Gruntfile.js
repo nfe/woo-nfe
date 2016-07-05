91c78b31d265f88d6a4f7ca8040914ba8f4ecb45
Renato Alves <espellcaste@gmail.com>
2016-07-05 10:19:27 -0300

    Check Text Domain Support

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,39 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		
+		// Check textdomain errors.
+		checktextdomain: {
+			options:{
+				text_domain: 'woocommerce-nfe',
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				]
+			},
+			files: {
+				src:  [
+					'**/*.php', // Include all files
+					'!apigen/**', // Exclude apigen/
+					'!node_modules/**', // Exclude node_modules/
+				],
+				expand: true
+			}
+		},
 
+		// Generate POT files.
 		makepot: {
 			target: {
 				options: {
@@ -16,7 +48,7 @@ module.exports = function(grunt) {
                     	'language': 'pt_BR',
                     	'last-translator': 'Renato Alves <contato@ralv.es',
 						'language-team': 'Renato Alves <contato@ralv.es',
-                    	'report-msgid-bugs-to': 'https://github.com/nfe/woocommerce-nfe',
+                    	'report-msgid-bugs-to': 'https://github.com/nfe/woocommerce-nfe/issues',
                     	'plural-forms': 'nplurals=2; plural=n != 1;',
 					    'x-poedit-country': 'Brasil',
 					    'x-poedit-language': 'Portuguese',
