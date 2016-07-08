95f9f270195e39627cc123c7af01f789ab7f242e
Renato Alves <contato@ralv.es>
2016-07-08 15:08:02 +0000

    Base files for unit testing

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class NFe_Test
+ *
+ * @package Woocommerce_NFe
+ */
+
+/**
+ * Sample test case.
+ */
+class NFe_Test extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
