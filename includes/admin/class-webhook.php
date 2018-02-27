adb03195f7fad9bc0eaff5d7753f2f4b1b6b8c76
Renato Alves <espellcaste@gmail.com>
2018-02-26 21:41:07 -0300

    Melhorando a classe responsável pelo webhook

--- includes/admin/class-webhook.php
+++ includes/admin/class-webhook.php
@@ -1,9 +1,7 @@
 <?php
 
-// Exit if accessed directly
+// Exit if accessed directly.
 defined('ABSPATH') || exit;
-
-if (!class_exists('WC_NFe_Webhook_Handler')) :
 
 /**
  * WooCommerce NFe WC_NFe_Webhook_Handler Class.
@@ -12,8 +10,8 @@ if (!class_exists('WC_NFe_Webhook_Handler')) :
  * @package  WooCommerce_NFe/Class/WC_NFe_Webhook_Handler
  * @version  1.0.4
  */
-class WC_NFe_Webhook_Handler
-{
+class WC_NFe_Webhook_Handler {
+
     /**
      * WC_Logger Logger instance.
      *
@@ -24,29 +22,27 @@ class WC_NFe_Webhook_Handler
     /**
      * Base Construct.
      */
-    public function __construct()
-    {
-        add_action('woocommerce_api_'.WC_API_CALLBACK, array($this, 'handle'));
+    public function __construct() {
+        add_action( 'woocommerce_api_' . WC_API_CALLBACK, array( $this, 'handle' ) );
     }
 
     /**
      * Handling incoming webhooks.
      */
-    public function handle()
-    {
-        $raw_body = file_get_contents('php://input');
-        $body = json_decode($raw_body);
+    public function handle() {
+        $raw_body = file_get_contents( 'php://input' );
+        $body = json_decode( $raw_body );
 
-        $this->logger(__('New webhook called.', 'woo-nfe') . " " . $raw_body);
+        $this->logger( __( 'New webhook called.', 'woo-nfe' ) . " " . $raw_body );
 
         try {
-            $this->process_event($body);
-        } catch (Exception $e) {
-            $this->logger(sprintf(__('Error: %s.', 'woo-nfe'), $e->getMessage()));
+            $this->process_event( $body );
+        } catch ( Exception $e ) {
+            $this->logger(sprintf( __( 'Error: %s.', 'woo-nfe' ), $e->getMessage() ) );
 
-            if (2 === $e->getCode()) {
-                http_response_code(422);
-                die($e->getMessage());
+            if ( 2 === $e->getCode() ) {
+                http_response_code( 422 );
+                die( $e->getMessage() );
             }
         }
     }
@@ -55,44 +51,47 @@ class WC_NFe_Webhook_Handler
      * Read json entity received and proccess the webhook.
      *
      * @param string $body
+     *
+     * @return void
      **/
-    private function process_event($body)
-    {
-        if (null == $body) {
-            throw new Exception(sprintf(__('Error while checking webhook JSON.', 'woo-nfe')));
+    protected function process_event( $body ) {
+        if ( is_null( $body ) ) {
+            throw new Exception(sprintf( __( 'Error while checking webhook JSON.', 'woo-nfe' ) ) );
         }
 
-        $this->logger(sprintf( __('New event procced.', 'woo-nfe') ));
+        $this->logger( sprintf( __('New event procced.', 'woo-nfe' ) ) );
 
-        $order = $this->find_order_by_nota_id($body->id);
+        $order = $this->get_order_by_nota_id( $body->id );
 
         $nfe = array(
-            'id' => $body->id,
-            'status' => $body->flowStatus,
-            'issuedOn' => $body->issuedOn,
+            'id'        => $body->id,
+            'status'    => $body->flowStatus,
+            'issuedOn'  => $body->issuedOn,
             'amountNet' => $body->amountNet,
             'checkCode' => $body->checkCode,
-            'number' => $body->number,
+            'number'    => $body->number,
         );
+
         update_post_meta($order->id, 'nfe_issued', $nfe);
 
-        $msg = sprintf( __('Order updated. Order: #%d NFe status: %s .', 'woo-nfe'), $order->id, $body->flowStatus );
-        $this->logger($msg);
-        $order->add_order_note($msg);
+        $msg = sprintf( __('Order updated. Order: #%d NFe status: %s .', 'woo-nfe' ), $order->id, $body->flowStatus );
+        $this->logger( $msg );
+        $order->add_order_note( $msg );
     }
 
     /**
-     * Find orders by NFe ID.
+     * Find orders by NFe.io ID.
      *
-     * @param Nota ID $id
+     * @param string $id NFE.io Receipt ID.
      *
-     * @return WC_Order
+     * @return WC_Order Order ID.
      */
-    private function find_order_by_nota_id($id) {
+    protected function get_order_by_nota_id( $id ) {
     	$issue_status = nfe_get_field( 'issue_when_status' );
 
-        $args = array(
+        $query_args = array(
             'post_type' => 'shop_order',
+            'post_status' => "wc-{$issue_status}",
             'meta_query' => array(
                 array(
                     'key' => 'nfe_issued',
@@ -100,35 +99,39 @@ class WC_NFe_Webhook_Handler
                     'compare' => 'LIKE',
                 ),
             ),
-            'post_status' => "wc-{$issue_status}",
         );
-        $query = new WP_Query($args);
 
-        if (false === $query->have_posts()) {
-            throw new Exception('Pedido com id de nota fiscal #'.$id.' não encontrado!', 2);
+        $query = new WP_Query( $query_args );
+
+        if ( false === $query->have_posts() ) {
+            throw new Exception( sprintf( __( 'Order with receipt number #%d not found.', 'woo-nfe' ), $id ), 2 );
         }
 
-        return nfe_wc_get_order($query->post->ID);
+        return nfe_wc_get_order( $query->post->ID );
     }
 
     /**
      * Logging method.
      *
      * @param string $message
+     *
+     * @return void
      */
-    public static function logger($message)
-    {
-        if (nfe_get_field('debug') == 'yes') {
-            if (empty(self::$logger)) {
+    public static function logger( $message ) {
+    	$debug = nfe_get_field( 'debug' );
+
+    	if ( empty( $debug ) ) {
+    		return;
+    	}
+
+        if ( $debug === 'yes' ) {
+            if ( empty( self::$logger ) ) {
                 self::$logger = new WC_Logger();
             }
-            self::$logger->add('nfe_webhook', $message);
+
+            self::$logger->add( 'nfe_webhook', $message );
         }
     }
 }
 
 return new WC_NFe_Webhook_Handler();
-
-endif;
-
-// That's it! =)
