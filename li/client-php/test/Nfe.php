b10e3780331d868f84f6fefd841ece3b197f0464
Ivo Linsmeyer Filho <linsmeyer@gmail.com>
2020-10-13 13:28:01 -0300

    Update Nfe.php

--- li/client-php/test/Nfe.php
+++ li/client-php/test/Nfe.php
@@ -1,11 +1,5 @@
 <?php
-$apiKey = "J6aFxW5rTe4uPKb0XtmWtJjJNR0aEtF0E4bq9KAqF8i8fGBgbIkqnW6irZn3pq8YJr1";
-
-putenv("NFE_API_KEY=$apiKey");
-
 $dir = dirname(__FILE__);
-
-//echo 'AQUI:'.$dir;
 
 include_once( $dir . '/../vendor/simpletest/simpletest/autorun.php');
 
@@ -16,7 +10,7 @@ echo 'Running NFe.io PHP Test Suite - ';
 include_once( $dir . '/../lib/init.php');
 include_once( $dir . '/../test/NFe/TestCase.php');
 include_once( $dir . '/../test/NFe/CompanyTestNew.php');
-//include_once( $dir . '/../test/NFe/LegalPersonTest.php');
-//include_once( $dir . '/../test/NFe/NaturalPersonTest.php');
-//include_once( $dir . '/../test/NFe/WebhookTest.php');
-//include_once( $dir . '/../test/NFe/ServiceInvoiceTest.php');
+include_once( $dir . '/../test/NFe/LegalPersonTest.php');
+include_once( $dir . '/../test/NFe/NaturalPersonTest.php');
+include_once( $dir . '/../test/NFe/WebhookTest.php');
+include_once( $dir . '/../test/NFe/ServiceInvoiceTest.php');
