f35bc33296f280d10b1cd8e569fea9c521105320
Ivo Linsmeyer Filho <linsmeyer@gmail.com>
2020-10-13 13:29:54 -0300

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,24 +1,13 @@
 <?php
 require_once 'helpers.php';
-
-//$wordpress_tests_lib =  dirname( dirname( __FILE__ ) ) ."/../../../wordpress-tests-lib";
 $wordpress_tests_lib =  dirname( dirname( __FILE__ ) );
-$apiKey = "J6aFxW5rTe4uPKb0XtmWtJjJNR0aEtF0E4bq9KAqF8i8fGBgbIkqnW6irZn3pq8YJr1";
-$companyId = "54244e0ee340420fdc94ad09";
-
-//$apiKey = "pvzwBy2Ak40b1mN1NOffLh6WFRtMpTpMcOi9ScvHfZpdgrcRZGw4ZShN0uxNqVoUgFi";
-//$companyId = "5ee800fd8a31980b9ca0cdb8";
-
+$apiKey = "";
+$companyId = "";
 putenv("WP_TESTS_DIR=$wordpress_tests_lib");
 putenv("NFE_API_KEY=$apiKey");
-
 define('COMPANY_ID', $companyId);
 
 $_tests_dir = getenv('WP_TESTS_DIR');
-
-//if (!$_tests_dir) {
-//	$_tests_dir = rtrim(sys_get_temp_dir(), '/\\') . '/wordpress-tests-lib';
-//}
 
 if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
