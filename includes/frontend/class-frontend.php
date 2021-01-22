a7384d73442b70a84c71a59ea40f26e2db488e36
JoaoVictorLNacional <viana.joao@linknacional.com>
2021-01-22 14:16:22 -0300

    modificações feitas conforme issue https://github.com/nfe/woo-nfe/pull/69

--- includes/frontend/class-frontend.php
+++ includes/frontend/class-frontend.php
@@ -1,180 +1,186 @@
 <?php
 
 // Exit if accessed directly
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-if ( ! class_exists('WC_NFe_FrontEnd') ) :
+if (!class_exists('WC_NFe_FrontEnd')) {
+    /**
+     * WooCommerce NFe WC_NFe_FrontEnd Class.
+     *
+     * @author   NFe.io
+     *
+     * @version  1.0.4
+     */
+    class WC_NFe_FrontEnd
+    {
+        /**
+         * Constructor.
+         *
+         * @since 1.0.4
+         */
+        public function __construct()
+        {
+            // Filters.
+            add_filter('woocommerce_my_account_my_orders_columns', [$this, 'nfe_column']);
+            add_filter('woocommerce_my_account_my_address_description', [$this, 'account_desc']);
 
-	/**
-	 * WooCommerce NFe WC_NFe_FrontEnd Class.
-	 *
-	 * @author   NFe.io
-	 * @package  WooCommerce_NFe/Class/WC_NFe_FrontEnd
-	 * @version  1.0.4
-	 */
-	class WC_NFe_FrontEnd {
+            // Actions.
+            add_action('woocommerce_my_account_my_orders_column_sales-receipt', [$this, 'column_content']);
+            add_action('woocommerce_order_details_after_order_table', [$this, 'column_content']);
+            add_action('woocommerce_before_edit_address_form_billing', [$this, 'billing_notice']);
+        }
 
-		/**
-		 * Constructor
-		 *
-		 * @since 1.0.4
-		 */
-		public function __construct() {
-			// Filters.
-			add_filter( 'woocommerce_my_account_my_orders_columns', [ $this, 'nfe_column' ] );
-			add_filter( 'woocommerce_my_account_my_address_description', [ $this, 'account_desc' ] );
+        /**
+         * Notice added on the WooCommerce edit-address page.
+         */
+        public function billing_notice()
+        {
+            if ('yes' === nfe_get_field('nfe_enable')) {
+                echo '<div class="woocommerce-message">'.esc_html__('The following address will <strong>also</strong> be used when issuing a NFe Sales Receipt.', 'woo-nfe').'</div>';
+            }
+        }
 
-			// Actions.
-			add_action( 'woocommerce_my_account_my_orders_column_sales-receipt', [ $this, 'column_content' ] );
-			add_action( 'woocommerce_order_details_after_order_table', [ $this, 'column_content' ] );
-			add_action( 'woocommerce_before_edit_address_form_billing', [ $this, 'billing_notice' ] );
-		}
+        /**
+         * Notice added in the My Account page.
+         *
+         * @return string
+         */
+        public function account_desc()
+        {
+            return esc_html__('The following address(es) will be used on the checkout page by default and also when issuing a NFe sales receipt.', 'woo-nfe');
+        }
 
-		/**
-		 * Notice added on the WooCommerce edit-address page
-		 *
-		 * @return void
-		 */
-		public function billing_notice() {
-			if ( nfe_get_field( 'nfe_enable' ) === 'yes' ) {
-				echo '<div class="woocommerce-message">' . esc_html__( 'The following address will <strong>also</strong> be used when issuing a NFe Sales Receipt.', 'woo-nfe' ) . '</div>';
-			}
-		}
+        /**
+         * NFe Column Header on Recent Orders.
+         *
+         * @param array $columns columns
+         *
+         * @return array
+         */
+        public function nfe_column($columns)
+        {
+            $new_column = [];
 
-		/**
-		 * Notice added in the My Account page
-		 *
-		 * @return string
-		 */
-		public function account_desc() {
-			return esc_html__( 'The following address(es) will be used on the checkout page by default and also when issuing a NFe sales receipt.', 'woo-nfe' );
-		}
+            foreach ($columns as $column_name => $column_info) {
+                $new_columns[$column_name] = $column_info;
 
-		/**
-		 * NFe Column Header on Recent Orders
-		 *
-		 * @param array $columns Columns.
-		 *
-		 * @return array
-		 */
-		public function nfe_column( $columns ) {
-			$new_column = array();
+                if ('order-total' === $column_name) {
+                    $new_columns['sales-receipt'] = esc_html__('Sales Receipt', 'woo-nfe');
+                }
+            }
 
-			foreach ( $columns as $column_name => $column_info ) {
-				$new_columns[ $column_name ] = $column_info;
+            return $new_columns;
+        }
 
-				if ( 'order-total' === $column_name ) {
-					$new_columns['sales-receipt'] = esc_html__( 'Sales Receipt', 'woo-nfe' );
-				}
-			}
+        /**
+         * NFe Sales Receipt Column Content on Recent Orders.
+         *
+         * @since 1.0.9
+         *
+         * @param WC_Order $order order object
+         */
+        public function column_content($order)
+        {
+            // Get order information.
+            $order_id = $order->get_id();
+            $nfe = get_post_meta($order_id, 'nfe_issued', true);
 
-			return $new_columns;
-		}
+            if ('yes' !== nfe_get_field('nfe_enable')) {
+                return;
+            }
 
-		/**
-		 * NFe Sales Receipt Column Content on Recent Orders
-		 *
-		 * @since 1.0.9
-		 *
-		 * @param WC_Order $order Order object.
-		 *
-		 * @return void
-		 */
-		public function column_content( $order ) {
-			// Get order information.
-			$order_id = $order->get_id();
-			$nfe      = get_post_meta( $order_id, 'nfe_issued', true );
+            // Build actions.
+            $actions = [];
 
-			if ( 'yes' !== nfe_get_field( 'nfe_enable' ) ) {
-				return;
-			}
+            if (!empty($nfe) && 'Cancelled' === $nfe['status']) {
+                $actions['woo_nfe_cancelled'] = [
+                    'url' => '#',
+                    'name' => __('NFe Cancelled', 'woo-nfe'),
+                    'action' => 'woo_nfe_cancelled',
+                ];
+            } elseif (!empty($nfe) && 'Issued' === $nfe['status']) {
+                $actions['woo_nfe_emitida'] = [
+                    'url' => '#',
+                    'name' => __('NFe Issued', 'woo-nfe'),
+                    'action' => 'woo_nfe_emitida',
+                ];
+            } elseif (!empty($nfe) && 'CancelledFailed' === $nfe['status']) {
+                $actions['woo_nfe_issue'] = [
+                    'url' => '#',
+                    'name' => __('NFe Cancelling Failed', 'woo-nfe'),
+                    'action' => 'woo_nfe_issue',
+                ];
+            } elseif (!empty($nfe) && 'IssueFailed' === $nfe['status']) {
+                $actions['woo_nfe_issue'] = [
+                    'url' => '#',
+                    'name' => __('NFe Issuing Failed', 'woo-nfe'),
+                    'action' => 'woo_nfe_issue',
+                ];
+            } elseif (!empty($nfe) && 'Processing' === $nfe['status']) {
+                $actions['woo_nfe_issue'] = [
+                    'url' => '#',
+                    'name' => __('NFe Processing', 'woo-nfe'),
+                    'action' => 'woo_nfe_issue',
+                ];
+            } elseif (!empty($nfe) && in_array($nfe['status'], nfe_processing_status(), true)) {
+                $actions['woo_nfe_issuing'] = [
+                    'url' => '#',
+                    'name' => __('Processing NFe', 'woo-nfe'),
+                    'action' => 'woo_nfe_issuing',
+                ];
+            } else {
+                if (!nfe_order_address_filled($order_id)) {
+                    $actions['woo_nfe_pending_address'] = [
+                        'url' => esc_url(wc_get_endpoint_url('edit-address')),
+                        'name' => esc_html__('Pending Address', 'woo-nfe'),
+                        'action' => 'woo_nfe_pending_address',
+                    ];
+                } elseif (!empty($nfe) && $nfe['id']) {
+                    $actions['woo_nfe_download'] = [
+                        'url' => wp_nonce_url(admin_url('admin-ajax.php?action=woocommerce_nfe_download&order_id='.$order_id), 'woo_nfe_download'),
+                        'name' => esc_html__('Download NFe', 'woo-nfe'),
+                        'action' => 'woo_nfe_download',
+                    ];
+                } else {
+                    if ('yes' === nfe_get_field('issue_past_notes')) {
+                        if (nfe_issue_past_orders($order) && empty($nfe['id'])) {
+                            $actions['woo_nfe_issue'] = [
+                                'url' => wp_nonce_url(admin_url('admin-ajax.php?action=woocommerce_nfe_issue&order_id='.$order_id), 'woo_nfe_issue'),
+                                'name' => esc_html__('Issue NFe', 'woo-nfe'),
+                                'action' => 'woo_nfe_issue',
+                            ];
+                        } else {
+                            $actions['woo_nfe_expired'] = [
+                                'url' => '#',
+                                'name' => esc_html__('Issue Expired', 'woo-nfe'),
+                                'action' => 'woo_nfe_expired',
+                            ];
+                        }
+                    } else {
+                        $actions['woo_nfe_issue'] = [
+                            'url' => wp_nonce_url(admin_url('admin-ajax.php?action=woocommerce_nfe_issue&order_id='.$order_id), 'woo_nfe_issue'),
+                            'name' => esc_html__('Issue NFe', 'woo-nfe'),
+                            'action' => 'woo_nfe_issue',
+                        ];
+                    }
+                }
+            }
 
-			// Build actions.
-			$actions = array();
+            if ('0.00' === $order->get_total()) {
+                $actions = [];
+            }
 
-			if ( ! empty( $nfe ) && 'Cancelled' === $nfe['status'] ) {
-				$actions['woo_nfe_cancelled'] = array(
-					'url'       => '#',
-					'name'      => __( 'NFe Cancelled', 'woo-nfe' ),
-					'action'    => 'woo_nfe_cancelled',
-				);
-			} elseif ( ! empty( $nfe ) && 'Issued' === $nfe['status'] ) {
-				$actions['woo_nfe_emitida'] = array(
-					'url'       => '#',
-					'name'      => __( 'NFe Issued', 'woo-nfe' ),
-					'action'    => 'woo_nfe_emitida',
-				);
-			} elseif ( ! empty( $nfe ) && 'CancelledFailed' === $nfe['status'] ) {
-				$actions['woo_nfe_issue'] = array(
-					'url'       => '#',
-					'name'      => __( 'NFe Cancelling Failed', 'woo-nfe' ),
-					'action'    => 'woo_nfe_issue',
-				);
-			} elseif ( ! empty( $nfe ) && 'IssueFailed' === $nfe['status'] ) {
-				$actions['woo_nfe_issue'] = array(
-					'url'       => '#',
-					'name'      => __( 'NFe Issuing Failed', 'woo-nfe' ),
-					'action'    => 'woo_nfe_issue',
-				);
-			} elseif ( ! empty( $nfe ) && in_array( $nfe['status'], nfe_processing_status(), true ) ) {
-				$actions['woo_nfe_issuing'] = array(
-					'url'       => '#',
-					'name'      => __( 'Processing NFe', 'woo-nfe' ),
-					'action'    => 'woo_nfe_issuing',
-				);
-			} else {
-				if ( ! nfe_order_address_filled( $order_id ) ) {
-					$actions['woo_nfe_pending_address'] = array(
-						'url'       => esc_url( wc_get_endpoint_url( 'edit-address' ) ),
-						'name'      => esc_html__( 'Pending Address', 'woo-nfe' ),
-						'action'    => 'woo_nfe_pending_address',
-					);
-				} elseif ( ! empty( $nfe ) && $nfe['id'] ) {
-					$actions['woo_nfe_download'] = array(
-						'url'       => wp_nonce_url( admin_url( 'admin-ajax.php?action=woocommerce_nfe_download&order_id=' . $order_id ), 'woo_nfe_download' ),
-						'name'      => esc_html__( 'Download NFe', 'woo-nfe' ),
-						'action'    => 'woo_nfe_download',
-					);
-				} else {
-					if ( nfe_get_field( 'issue_past_notes' ) === 'yes' ) {
-						if ( nfe_issue_past_orders( $order ) && empty( $nfe['id'] ) ) {
-							$actions['woo_nfe_issue'] = array(
-								'url'       => wp_nonce_url( admin_url( 'admin-ajax.php?action=woocommerce_nfe_issue&order_id=' . $order_id ), 'woo_nfe_issue' ),
-								'name'      => esc_html__( 'Issue NFe', 'woo-nfe' ),
-								'action'    => 'woo_nfe_issue',
-							);
-						} else {
-							$actions['woo_nfe_expired'] = array(
-								'url'       => '#',
-								'name'      => esc_html__( 'Issue Expired', 'woo-nfe' ),
-								'action'    => 'woo_nfe_expired',
-							);
-						}
-					} else {
-						$actions['woo_nfe_issue'] = array(
-							'url'       => wp_nonce_url( admin_url( 'admin-ajax.php?action=woocommerce_nfe_issue&order_id=' . $order_id ), 'woo_nfe_issue' ),
-							'name'      => esc_html__( 'Issue NFe', 'woo-nfe' ),
-							'action'    => 'woo_nfe_issue',
-						);
-					}
-				}
-			}
+            foreach ($actions as $action) {
+                printf(
+                    '<a class="button view %s" href="%s" data-tip="%s">%s</a>',
+                    esc_attr($action['action']),
+                    esc_url($action['url']),
+                    esc_attr($action['name']),
+                    esc_attr($action['name'])
+                );
+            }
+        }
+    }
 
-			if ( '0.00' === $order->get_total() ) {
-				$actions = array();
-			}
-
-			foreach ( $actions as $action ) {
-				printf( '<a class="button view %s" href="%s" data-tip="%s">%s</a>',
-					esc_attr( $action['action'] ),
-					esc_url( $action['url'] ),
-					esc_attr( $action['name'] ),
-					esc_attr( $action['name'] )
-				);
-			}
-		}
-	}
-
-	return new WC_NFe_FrontEnd();
-
-endif;
+    return new WC_NFe_FrontEnd();
+}
--- li/client-php/lib/NFe/NFe.php
+++ li/client-php/lib/NFe/NFe.php
@@ -1,62 +1,79 @@
 <?php
 
-abstract class NFe_io {
+abstract class NFe_io
+{
+    const VERSION = '2.0.0';
 
-  // @var string The NFe API key to be used for requests.
-  public static $api_key = null;
+    // @var string The NFe API key to be used for requests.
+    public static $api_key = null;
 
-  // @var string The version of the NFe API to use for requests.
-  public static $api_version = 'v1';
+    // @var string The version of the NFe API to use for requests.
+    public static $api_version = 'v1';
 
-  // @var string The base URL for the NFe API.
-  public static $endpoint = 'https://api.nfe.io';
+    // @var string The base URL for the NFe API.
+    public static $endpoint = 'https://api.nfe.io';
 
-  // @var boolean Defaults to true.
-  public static $verifySslCerts = true;
+    // @var boolean Defaults to true.
+    public static $verifySslCerts = true;
 
-  // @var boolean Defaults to false
-  public static $debug = false;
+    // @var boolean Defaults to false
+    public static $debug = false;
+    public static $pdf = false;
 
-  const VERSION = '2.0.0';
+    /**
+     * Sets the Base URI to be sued for requests.
+     *
+     * @return string
+     */
+    public static function getBaseURI()
+    {
+        return self::$endpoint.'/'.self::$api_version;
+    }
 
-  /**
-   * Sets the Base URI to be sued for requests
-   *
-   * @return string
-   */
-  public static function getBaseURI() {
-    return self::$endpoint . '/' . self::$api_version;
-  }
+    /**
+     * Sets the Host to be used for requests.
+     *
+     * @param string $host Base URI
+     */
+    public static function setHost($host)
+    {
+        self::$endpoint = $host;
+    }
 
-  /**
-   * Sets the Host to be used for requests
-   *
-   * @param string $host Base URI
-   */
-  public static function setHost( $host ) {
-    self::$endpoint = $host;
-  }
+    /**
+     * Sets the API key to be used for requests.
+     *
+     * @param string $apiKey
+     * @param mixed  $_api_key
+     */
+    public static function setApiKey($_api_key)
+    {
+        self::$api_key = $_api_key;
+    }
 
-  /**
-   * Sets the API key to be used for requests.
-   *
-   * @param string $apiKey
-   */
-  public static function setApiKey( $_api_key ) {
-    self::$api_key = $_api_key;
-  }
+    /**
+     * @return string the API key used for requests
+     */
+    public static function getApiKey()
+    {
+        return self::$api_key;
+    }
 
-  /**
-  * @return string The API key used for requests.
-  */
-  public static function getApiKey() {
-    return self::$api_key;
-  }
+    /**
+     * @return bool
+     */
+    public static function getVerifySslCerts()
+    {
+        return self::$verifySslCerts;
+    }
 
-  /**
-   * @return boolean
-   */
-  public static function getVerifySslCerts() {
-      return self::$verifySslCerts;
-  }
+    public static function setPdf($bol)
+    {
+        self::$pdf = $bol;
+    }
+
+    public static function getPdf()
+    {
+        return self::$pdf;
+    }
 }
