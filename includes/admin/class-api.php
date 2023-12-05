0803a8d74d507f9aba6be70a61d0459886aa3dc8
Luiz NFE.io <74252574+luiznfeio@users.noreply.github.com>
2023-12-05 08:51:07 -0300

    Adicionando externalId na chamada à API - evitar duplicidade de nfse

--- includes/admin/class-api.php
+++ includes/admin/class-api.php
@@ -241,6 +241,7 @@ if ( ! class_exists( 'NFe_Woo' ) ) {
 				'description'        => $servicesDescription,
 				'servicesAmount'     => $servicesAmount,
 				'borrower'           => $borrower,
+				'externalId'		 => $order_id,
 			);
 
 			// Removes empty, false and null fields from the array.
